94298efa20d12e15d3733dbdc9f79668fdb21964
Parnell Springmeyer <parnell@ixmat.us>
2013-11-25 15:29:57 -0600

    Cleaning up the types quite a bit

--- src/Database/Tempodb/Methods/Write.hs
+++ src/Database/Tempodb/Methods/Write.hs
@@ -4,87 +4,19 @@ module Database.Tempodb.Methods.Write
 ( writeOne
 , writeBulk
 , writeMulti
-, SeriesDataWrite(..)
-, Bulk(..)
 )
 where
 
-import           Control.Applicative
 import           Control.Monad.Reader
 import           Data.Aeson             as A
-import           Data.Aeson.Types       (Parser)
 import           Data.ByteString.Char8  as C8
 import           Data.ByteString.Lazy   (fromStrict, toStrict)
 import           Data.Monoid
-import qualified Data.Text              as T
-import           Data.Time
-import qualified Database.Tempodb.Types as T
+import           Database.Tempodb.Types as T
 import           Database.Tempodb.Util
 import           Network.Http.Client
-import           System.Locale
 
-data SeriesDataWrite = SeriesDataWrite
-    { uid          :: T.IdOrKey
-    , timestampSDW :: Maybe UTCTime
-    , valueSDW     :: Double
-    } deriving (Show, Eq, Ord)
-
-data Bulk = Bulk
-    { timestampB :: UTCTime
-    , valuesB    :: [SeriesDataWrite]
-    } deriving (Show, Eq, Ord)
-
-instance FromJSON Bulk where
-    parseJSON (Object o) = Bulk     <$>
-                           o .: "t" <*>
-                           o .: "data"
-    parseJSON _ = mzero
-
-instance ToJSON Bulk where
-    toJSON (Bulk t v) = object
-        [ "t"    .= t
-        , "data" .= v
-        ]
-
-instance FromJSON SeriesDataWrite where
-    parseJSON = parseSeriesBulk
-
-parseSeriesBulk :: Value -> Parser SeriesDataWrite
-parseSeriesBulk v = do
-    case v of
-        Object o -> do
-            -- Is the ID there? Or is the key element there?
-            kid <- (o .:? "id") >>= isUID o
-            ts  <- o .:? "t"
-            val <- o .: "v"
-            return $ SeriesDataWrite kid ts val
-        _ -> mzero
-  where
-    isUID o i = case i of
-        Nothing    -> (o .:? "key") >>= isKEY
-        Just idval -> return $ T.SeriesId idval
-    isKEY k = case k of
-        Nothing    -> fail ((show v) ++ " is not a valid object.")
-        Just idkey -> return $ T.SeriesKey idkey
-
-instance ToJSON SeriesDataWrite where
-    toJSON = buildSeriesBulk
-
-buildSeriesBulk :: SeriesDataWrite -> Value
-buildSeriesBulk (SeriesDataWrite i t v) = object . ts $
-    [ eid
-    , "v" .= v
-    ]
-  where
-    ts l = case t of
-        Nothing -> l
-
-        Just tv -> ("t", String . T.pack $ formatTime defaultTimeLocale "%FT%H:%M:%S%Q%z" tv):l
-    eid = case i of
-        T.SeriesId idv -> "id" .= idv
-        T.SeriesKey kv -> "key".= kv
-
-writeOne :: T.IdOrKey -> T.Data -> T.Tempodb Bool
+writeOne :: IdOrKey -> Data -> Tempodb Bool
 writeOne q d = do
     let postdata = toStrict $ A.encode d
     auth <- ask
@@ -102,7 +34,7 @@ writeOne q d = do
     ident (T.SeriesKey k) = "/key/" <> k
     path = rootpath <> "/series" <> (ident q)
 
-writeBulk :: Bulk -> T.Tempodb Bool
+writeBulk :: Bulk -> Tempodb Bool
 writeBulk d = do
     let postData = toStrict $ A.encode d
     auth <- ask
@@ -115,7 +47,7 @@ writeBulk d = do
 
     return True
 
-writeMulti :: [SeriesDataWrite] -> T.Tempodb (Either (Maybe [SeriesDataWrite]) Bool)
+writeMulti :: [Data] -> T.Tempodb (Either (Maybe [Data]) Bool)
 writeMulti d = do
     let postData = toStrict $ A.encode d
     auth <- ask
--- src/Database/Tempodb/Types.hs
+++ src/Database/Tempodb/Types.hs
@@ -7,6 +7,7 @@ import           Control.Applicative
 import           Control.Monad
 import           Control.Monad.Reader
 import           Data.Aeson
+import           Data.Aeson.Types      (Parser)
 import           Data.ByteString.Char8 as C8
 import           Data.Map              (Map)
 import qualified Data.Text             as T
@@ -66,8 +67,14 @@ instance ToJSON Series where
         ]
 
 data Data = Data
-    { timestamp :: UTCTime
+    { uid       :: Maybe IdOrKey
+    , timestamp :: Maybe UTCTime
     , value     :: Double
+    } deriving (Show, Eq, Ord)
+
+data Bulk = Bulk
+    { timestmp   :: UTCTime
+    , bulkValues :: [Data]
     } deriving (Show, Eq, Ord)
 
 data Rollup = Rollup
@@ -106,22 +113,17 @@ instance FromJSON SeriesData where
 
     parseJSON _ = mzero
 
-instance FromJSON Data where
-    parseJSON (Object o) = Data     <$>
+instance FromJSON Bulk where
+    parseJSON (Object o) = Bulk     <$>
                            o .: "t" <*>
-                           o .: "v"
+                           o .: "data"
     parseJSON _ = mzero
 
-instance ToJSON Data where
-    toJSON = buildData
-
-buildData :: Data -> Value
-buildData (Data t v) = object
-    [ timest
-    , "v" .= v
-    ]
-  where
-    timest = ("t", String . T.pack $ formatTime defaultTimeLocale "%FT%H:%M:%S%Q%z" t)
+instance ToJSON Bulk where
+    toJSON (Bulk t v) = object
+        [ "t"    .= t
+        , "data" .= v
+        ]
 
 instance FromJSON Rollup where
     parseJSON (Object o) = Rollup          <$>
@@ -140,3 +142,39 @@ instance FromJSON Summary where
                            o .: "ss"     <*>
                            o .: "count"
     parseJSON _ = mzero
+
+instance FromJSON Data where
+    parseJSON = parseSeriesData
+
+parseSeriesData :: Value -> Parser Data
+parseSeriesData v = do
+    case v of
+        Object o -> do
+            -- Is the ID there? Or is the key element there?
+            kid <- (o .:? "id") >>= isUID o
+            ts  <- o .:? "t"
+            val <- o .: "v"
+            return $ Data kid ts val
+        _ -> mzero
+  where
+    isUID o i = case i of
+        Nothing    -> (o .:? "key") >>= isKEY
+        Just idval -> return . Just $ SeriesId idval
+    isKEY k = case k of
+        Nothing    -> return Nothing
+        Just idkey -> return . Just $ SeriesKey idkey
+
+instance ToJSON Data where
+    toJSON = buildSeriesData
+
+buildSeriesData :: Data -> Value
+buildSeriesData (Data i t v) = object . ts .eid $ ["v" .= v]
+  where
+    ts l = case t of
+        Nothing -> l
+
+        Just tv -> ("t", String . T.pack $ formatTime defaultTimeLocale "%FT%H:%M:%S%Q%z" tv):l
+    eid l = case i of
+        Nothing -> l
+        Just (SeriesId idv) -> ("id" .= idv):l
+        Just (SeriesKey kv) -> ("key".= kv) :l
