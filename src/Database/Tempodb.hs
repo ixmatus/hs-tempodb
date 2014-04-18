a4018e7fb185b3ba16476d91644fd1c0e9f046ec
Parnell Springmeyer <parnell@ixmat.us>
2014-04-18 18:22:01 -0500

    Adding a "disclaimer"

--- src/Database/Tempodb.hs
+++ src/Database/Tempodb.hs
@@ -1,5 +1,14 @@
-{-# LANGUAGE OverloadedStrings #-}
-
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Database.Tempodb
+-- Copyright   :  (C) 2013 Parnell Springmeyer
+-- License     :  BSD3
+-- Maintainer  :  Parnell Springmeyer <parnell@ixmat.us>
+-- Stability   :  stable
+--
+-- Please familiarize yourself with the <https://tempo-db.com/docs/api/ TempoDB REST API>. The
+-- `Database.Tempodb.Methods` mimic TempoDB's REST endpoints.
+----------------------------------------------------------------------------
 module Database.Tempodb
 ( module Database.Tempodb.Types
 , module Database.Tempodb.Methods
